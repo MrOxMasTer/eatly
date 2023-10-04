b4fd3084437ac6c4812727320cfa86bbe76ec4bc
MrOxMasTer <59291123+MrOxMasTer@users.noreply.github.com>
2023-10-05 00:58:34 +0300

    The approach to obtaining data from the server occurs

--- server/src/restaurants/restaurants.controller.ts
+++ server/src/restaurants/restaurants.controller.ts
@@ -20,53 +20,53 @@ import { RestaurantsService } from './restaurants.service';
 
 @Controller('restaurants')
 export class RestaurantsController {
-    constructor(private restaurantsService: RestaurantsService) {}
+  constructor(private restaurantsService: RestaurantsService) {}
 
-    @Post('create')
-    @UseInterceptors(FileInterceptor('image'))
-    async createRestaurant(
-        @Body() dto: CreateRestaurantDto,
-        @UploadedFile() image: Express.Multer.File,
-    ) {
-        return this.restaurantsService.createRestaurant(dto, image);
-    }
+  @Post('create')
+  @UseInterceptors(FileInterceptor('image'))
+  async createRestaurant(
+    @Body() dto: CreateRestaurantDto,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    return this.restaurantsService.createRestaurant(dto, image);
+  }
 
-    @Get('')
-    async getRestaurants() {
-        return this.restaurantsService.getRestaurants();
-    }
+  @Get('')
+  async getRestaurants() {
+    return this.restaurantsService.getRestaurants();
+  }
 
-    @Get('top')
-    async getTopRestaurants() {
-        return this.restaurantsService.getTopRestaurants();
-    }
+  @Get('top')
+  async getTopRestaurants() {
+    return this.restaurantsService.getTopRestaurants();
+  }
 
-    @Get('menu')
-    async getRestaurantsByForm(@Query() dto: GetRestaurantsByForm) {
-        const restaurant = await this.restaurantsService.getRestaurantsByForm({
-            ...dto,
-        });
+  @Get('menu')
+  async getRestaurantsByForm(@Query() dto: GetRestaurantsByForm) {
+    const restaurant = await this.restaurantsService.getRestaurantsByForm({
+      ...dto,
+    });
 
-        return restaurant;
-    }
+    return restaurant;
+  }
 
-    @Get(':id')
-    async getRestaurantById(@Param('id') id: string) {
-        return this.restaurantsService.getRestaurantById(id);
-    }
+  @Get(':id')
+  async getRestaurantById(@Param('id') id: string) {
+    return this.restaurantsService.getRestaurantById(id);
+  }
 
-    @Delete('delete/:id')
-    async deleteRestaurant(@Param('id') id: string) {
-        return this.restaurantsService.deleteRestaurant({ id });
-    }
+  @Delete('delete/:id')
+  async deleteRestaurant(@Param('id') id: string) {
+    return this.restaurantsService.deleteRestaurant({ id });
+  }
 
-    @Patch('addStatus')
-    async addStatustTheRestaurant(@Body() dto: AddStatustRestaurant) {
-        return this.restaurantsService.addStatustTheRestaurant(dto);
-    }
+  @Patch('addStatus')
+  async addStatustTheRestaurant(@Body() dto: AddStatustRestaurant) {
+    return this.restaurantsService.addStatustTheRestaurant(dto);
+  }
 
-    @Patch('removeStatus')
-    async removeStatusTheRestaurant(@Body() dto: AddStatustRestaurant) {
-        return this.restaurantsService.removeStatustTheRestaurant(dto);
-    }
+  @Patch('removeStatus')
+  async removeStatusTheRestaurant(@Body() dto: AddStatustRestaurant) {
+    return this.restaurantsService.removeStatustTheRestaurant(dto);
+  }
 }
